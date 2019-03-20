5e3c544608a506621566cafc3f1def5aeeca50d4
syam fauzy <msyamf@gmail.com>
2019-03-21 02:53:16 +0700

    [update & add] update Handle the back button & plugin add cordova-plugin-statusbar

--- platforms/android/app/build/intermediates/assets/debug/www/js/app.js
+++ platforms/android/app/build/intermediates/assets/debug/www/js/app.js
@@ -1,5 +1,50 @@
+
+
+
+
+
+document.addEventListener("backbutton", onBackKeyDown, false);
+
+
+// Handle the back button
+// function onBackKeyDown(url) {
+//   if(url=='/'){
+//     navigator.app.exitApp();
+//   }else{
+//     $$('[class="link back"]').click()
+//   }
+// }
+// function exit(){
+//   navigator.app.exitApp();
+// }
+
+
+function onLoad() {
+  document.addEventListener("deviceready", onDeviceReady, false);
+}
+
+// Cordova is loaded and it is now safe to call Cordova methods
+function onDeviceReady() {
+  // Register the event listener
+  document.addEventListener("backbutton", onBackKeyDown, false);
+}
+
+// Handle the back button
+function onBackKeyDown() {
+  if(app.form.getFormData('page')=='/'){
+    app.dialog.confirm('Are you sure you want to exit?','Exit. . .', function () {
+      navigator.app.exitApp();
+    });
+    
+  }else{
+    app.form.storeFormData('page','/');
+    $$('[class="link back"]').click()
+  }
+}
+
+
 // Dom7
-var $ = Dom7;
+var $$ = Dom7;
 
 // Theme
 var theme = 'auto';
@@ -12,6 +57,7 @@ var app = new Framework7({
   id: 'io.framework7.testapp',
   root: '#app',
   theme: theme,
+  pushState : true,
   data: function () {
     return {
       user: {
@@ -25,8 +71,20 @@ var app = new Framework7({
       app.dialog.alert('Hello World!');
     },
   },
+  
   routes: routes,
   vi: {
     placementId: 'pltd4o7ibb9rc653x14',
   },
+  
 });
+
+$$(document).on('page:init',function (e) {
+  app.form.storeFormData('page',e.detail.router.url);
+  console.log(app.form.getFormData('page'))
+  console.log(e.detail.router)
+})
+
+$$(document).on('page:init', '.page[data-name="p404"]', function (e) {
+  //onBackKeyDown(e.detail.router.url);
+})
--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -1,5 +1,49 @@
+
+
+
+
+
+document.addEventListener("backbutton", onBackKeyDown, false);
+
+
+// Handle the back button
+// function onBackKeyDown(url) {
+//   if(url=='/'){
+//     navigator.app.exitApp();
+//   }else{
+//     $$('[class="link back"]').click()
+//   }
+// }
+// function exit(){
+//   navigator.app.exitApp();
+// }
+
+
+function onLoad() {
+  document.addEventListener("deviceready", onDeviceReady, false);
+}
+
+// Cordova is loaded and it is now safe to call Cordova methods
+//
+function onDeviceReady() {
+  // Register the event listener
+  document.addEventListener("backbutton", onBackKeyDown, false);
+}
+
+// Handle the back button
+//
+function onBackKeyDown() {
+
+  if(app.form.getFormData('page')=='/'){
+    navigator.app.exitApp();
+  }else{
+    app.form.storeFormData('page','/');
+    $$('[class="link back"]').click()
+  }
+//app.dialog.alert('Hello World!');
+}
 // Dom7
-var $ = Dom7;
+var $$ = Dom7;
 
 // Theme
 var theme = 'auto';
@@ -12,6 +56,7 @@ var app = new Framework7({
   id: 'io.framework7.testapp',
   root: '#app',
   theme: theme,
+  pushState : true,
   data: function () {
     return {
       user: {
@@ -25,8 +70,20 @@ var app = new Framework7({
       app.dialog.alert('Hello World!');
     },
   },
+  
   routes: routes,
   vi: {
     placementId: 'pltd4o7ibb9rc653x14',
   },
+  
 });
+
+$$(document).on('page:init',function (e) {
+  app.form.storeFormData('page',e.detail.router.url);
+  console.log(app.form.getFormData('page'))
+  console.log(e.detail.router)
+})
+
+$$(document).on('page:init', '.page[data-name="p404"]', function (e) {
+  //onBackKeyDown(e.detail.router.url);
+})
